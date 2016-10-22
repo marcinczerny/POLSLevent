939679475a9d0ac8318450313886dd4e0988166b
root <czernymarcin@gmail.com>
2016-10-22 23:54:19 +0200

    <Message>

--- client/main.js
+++ client/main.js
@@ -1,22 +1,10 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
+import { Accounts } from 'meteor/accounts-base';
+import { Template } from 'meteor/templating';
 
-//Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  //this.counter = new ReactiveVar(0);
-//});
+Accounts.ui.config({
 
-Template.body.helpers({
-  resolutions: [
-	{title: "Hello Resolution #1"}
-	],
+  passwordSignupFields: 'USERNAME_ONLY',
+
 });
 
-//Template.hello.events({
-  //'click button'(event, instance) {
-    // increment the counter when button is clicked
-    //instance.counter.set(instance.counter.get() + 1);
-  //},
-//});
